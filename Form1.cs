bf06dd80e8366b6f324eac136d4c28c1e69dfafa
Ravi Waghmare <ravikantwaghmare82@gmail.com>
2023-01-31 15:17:58 +0530

    first commit

---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeCrudOperationLinq
+{
+    public partial class Form1 : Form
+    {
+        EmployeeDBDataContext dc;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        public void LoadData()
+        {
+            dc = new EmployeeDBDataContext();
+            dataGridView1.DataSource = dc.Employees;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          LoadData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f=new Form2();
+            f.ShowDialog();
+            LoadData();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(dataGridView1.SelectedRows.Count>0)
+            {
+            Form2 f=new Form2();
+            f.textBox1.ReadOnly = true;
+                f.button2.Enabled = false;
+                f.button1.Text = "Update";
+            f.textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            f.textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            f.textBox3.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            f.textBox4.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            f.ShowDialog();
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record for updation!!","Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                if(MessageBox.Show("Are youy sure of deleting the selected record?", "Confiramation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    int eno = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+                    Employee obj = dc.Employees.SingleOrDefault(E => E.Id == eno);
+                    dc.Employees.DeleteOnSubmit(obj);
+                    dc.SubmitChanges();
+                    LoadData();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record for deletion!!!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeCrudOperationLinq
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          EmployeeDBDataContext dc=new EmployeeDBDataContext();
+            if (textBox1.ReadOnly == false)
+            { 
+             Employee obj=new Employee();
+            obj.Id=int.Parse(textBox1.Text);
+            obj.Name=textBox2.Text;
+            obj.Job=textBox3.Text;
+            obj.Salary=int.Parse(textBox4.Text);
+            dc.Employees.InsertOnSubmit(obj);
+            dc.SubmitChanges();
+            MessageBox.Show("Record inserted to the table.");
+            }
+            else
+            {
+                Employee obj=dc.Employees.SingleOrDefault(E=>E.Id==int.Parse(textBox1.Text));
+                obj.Name =textBox2.Text;
+                obj.Job=textBox3.Text;
+                obj.Salary =int.Parse(textBox1.Text);
+                dc.SubmitChanges();
+                MessageBox.Show("Record Updated Successfully!!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach(Control ctrl in this.Controls)
+            {
+                if(ctrl is TextBox)
+                {
+                    TextBox tb = ctrl as TextBox;
+                    tb.Clear();
+                }
+            }
+            textBox1.Focus();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
